98d39f1aa0b38087ebb4f472c6555fb4a36cc860
Ripudaman Singh <ripudamansingh1302@gmail.com>
2026-05-22 09:34:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-// 1. Import Firebase tools directly from the web
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, serverTimestamp, query, orderBy } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBLUrj9EtCJDxmzvAFiG2qjMM41vgDvs6A",
   authDomain: "bluribus-fd.firebaseapp.com",
@@ -20,9 +12,6 @@ const firebaseConfig = {
   measurementId: "G-48RSC5EB98"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // 3. Start up Firebase
 const app = initializeApp(firebaseConfig);
